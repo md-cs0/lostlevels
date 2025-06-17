c522c1fbb3282a8cf3415e9751f3f0c3f2bd8181
md-cs0 <200961600+md-cs0@users.noreply.github.com>
2025-06-17 22:00:59 +0100

    Bump level count back up to 12
    This consists of 4 levels for 3 separate world portals. At the
    current moment, the game still only has 2 levels for the first
    world portal. Thus, loading any levels beyond world 1-2 for
    world 1 and any level from the other two world portals will
    crash the game.

--- lostlevels/levelinfo.py
+++ lostlevels/levelinfo.py
@@ -1,5 +1,5 @@
 """A list of constants that define some information about
 the Lost Levels levels."""
 
-NUM_WORLDS  = 1
-NUM_LEVELS  = 2
+NUM_WORLDS  = 3
+NUM_LEVELS  = 4
--- lostlevels/savefile.py
+++ lostlevels/savefile.py
@@ -37,7 +37,7 @@ class LLSV():
     def __init__(self, name):
         self.name = name
         self.header = LLSVHeader()
-        self.currentlevel = [1]
+        self.currentlevel = [1] * levelinfo.NUM_WORLDS
     
     # Read from an existing save file.
     def read(self, savedir = ""):
